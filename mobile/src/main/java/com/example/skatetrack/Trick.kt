e48d9234aa645c0f899f9d435d90b5b6be7395ee
Patrick G-B <pgardnerbrunton@ct.com.au>
2024-06-23 18:10:10 +1000

    Added attempt and land history for each trick

--- mobile/src/main/java/com/example/skatetrack/Trick.kt
+++ mobile/src/main/java/com/example/skatetrack/Trick.kt
@@ -1,7 +1,19 @@
 package com.example.skatetrack
 
+import java.util.Date
+
+data class Attempt(
+    val date: Date
+)
+
+data class Land(
+    val date: Date
+)
+
 data class Trick(
     val stance: String,
     val trick: String,
-    val landingGoal: Int
+    val landingGoal: Int,
+    val lands: MutableList<Land> = mutableListOf(),
+    val attempts: MutableList<Attempt> = mutableListOf()
 )
